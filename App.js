342b36d111b1796e3beddf1472433be6a617c8b3
komLL <99006048+komLL@users.noreply.github.com>
2022-10-07 13:36:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from './component/Title'
+import User from './component/User'
+
+function App() {
+  return (
+    <div>
+    <Title/>
+    <User/>
+    </div>
+  )
+}
+
+export default App
